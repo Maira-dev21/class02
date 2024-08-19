d293ce598216aab01c73f9447379e5f3e9c7934f
Maira Tariq <mairatariq896@gmail.com>
2024-08-18 17:32:17 -0700

    add code

---
+++ 02.py
@@ -0,0 +1,114 @@
+# variable
+userName:str ="maira tariq"
+print(userName)
+# datatype
+# num1:int = 10
+# print(num1)
+# 
+#  userName:int = 10       #camelcase
+# user_name:int = 60      #sankecase
+# UserName:int = 30       #pascalcase
+# USERNAME:int = 69       #uppercase
+# username:int = 30       #smallcase
+#
+# userName:str = "samra tariq"
+#lines of code is called program
+#set of programs is called software
+
+# float data type
+# num:int = 30
+# num2 : float = 50.6 
+# num3 :bool = False
+#
+num1 :int = 48
+num2 :int = 30
+num3 = num1 + num2
+print(num3)
+# Arithematic operation
+#  multiplication , division
+# add , subtract
+num4 : int = 5
+num5 : int = 10
+num3 : int =  -6+10/5*25
+print(num3) 
+
+firstname :str = "maira"
+lastname :str = "tariq"
+Fullname :str = firstname + lastname
+print(Fullname)
+ #boolean
+result:bool = 10 < 5
+print(result) 
+
+a,b,c = 10,200,30 #multiple value assign with multiple variable
+
+a = b = c = 300  #single value assign with multiple variable
+
+# to find address of variable with id
+num10 = 400
+print(id(num10))
+# type casting
+print(type(num10))
+
+# remainder modulus
+r = 30%2
+print(r)
+# if divendent value is smaller than divider
+f = 2%30
+print(f)
+
+# power square
+g = 2**4
+print(g)
+
+# floor division
+h = 10//3
+print(h)
+
+# add value to variable (add & assign)
+d = 30
+d += 5 
+print(d)
+
+
+# delete variable
+# num21 = 200
+# del num21
+# print(num21)
+
+# modulus , floor division or exponential (who come first) , 
+# division or multiplication , add or sum
+#  num10 = 3=3-10**4//2>4 (answer in true or false)
+
+# comparison operators
+sam = 4>=5
+print(sam)
+
+#  For AND operator
+# T and T = T
+# T and F = F
+# F and T = F
+# F and F = F
+
+#  For OR operator
+# T or T = T
+# T or F = T
+# F or T = T
+# F or F = F
+
+# For Not operator
+# !T = F
+# !F = T
+
+user = int(input("hello samra , enter a number"))
+output = user + 3
+print (output)
+
+#  integer to string
+num60 = 45
+num65 = str(num60)
+print(num65)
+
+user2:str = int(input("enter a number"))
+output2 = int(user2) - 5
+print (output2)
